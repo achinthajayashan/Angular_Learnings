aadeb01d676a741e6d8259b693ab13943675bc4a
achinthajayashan <achintha.uns.2018@gmail.com>
2024-03-05 07:25:38 +0530

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet, HomeComponent],
+  // templateUrl: './app.component.html',
+  template: `
+      <main>
+          <header class="headerSec">
+              <h1>This is Header</h1>
+          </header>
+          <section class="content">
+              <app-home></app-home>
+          </section>
+      </main>
+  `,
+  // styleUrl: './app.component.css',
+  styles:`
+  .headerSec{
+   background-color: black;
+   width: 100vw;
+   height: 7vh;
+   color: white;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+  }
+  `
 })
 export class AppComponent {
   title = 'angular-learnings';

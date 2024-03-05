e40f2e598de0b96a4de2062eba97e855d3230c3d
achinthajayashan <achintha.uns.2018@gmail.com>
2024-03-05 23:01:27 +0530

    setupped bootstrap successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {HeaderComponent} from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
-  // templateUrl: './app.component.html',
-  template: `
-      <main>
-          <header class="headerSec">
-              <h1>This is Header</h1>
-          </header>
-          <section class="content">
-              <app-home></app-home>
-          </section>
-      </main>
-  `,
-  // styleUrl: './app.component.css',
-  styles:`
-  .headerSec{
-   background-color: black;
-   width: 100vw;
-   height: 7vh;
-   color: white;
-   display: flex;
-   justify-content: center;
-   align-items: center;
-  }
-  `
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-learnings';

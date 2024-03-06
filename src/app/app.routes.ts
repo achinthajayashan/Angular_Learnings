7434232246a2c0be31b90857d57ea7b8cedf98cb
achinthajayashan <achintha.uns.2018@gmail.com>
2024-03-06 08:02:49 +0530

    routing completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
+import {IntroductionComponent} from "./introduction/introduction.component";
+import {ComponentsComponent} from "./components/components.component";
+import {RoutingsComponent} from "./routings/routings.component";
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: '', component: IntroductionComponent },
+  { path: 'components', component: ComponentsComponent },
+  { path: 'routings', component: RoutingsComponent }
 ];
---
+++ src/app/routings/routings.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-routings',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      routings works!
+    </p>
+  `,
+  styleUrl: './routings.component.css'
+})
+export class RoutingsComponent {
+
+}

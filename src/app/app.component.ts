d58a750ff0e89f5a55ee9acaa79810c9d13b4e49
achinthajayashan <achintha.uns.2018@gmail.com>
2024-03-07 07:47:45 +0530

    ng classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {HeaderComponent} from "./header/header.component";
 import {FooterComponent} from "./footer/footer.component";
+import {NgClassesComponent} from "./ng-classes/ng-classes.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent , NgClassesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,12 @@ import {AboutComponent} from "./about/about.component";
 import {IntroductionComponent} from "./introduction/introduction.component";
 import {ComponentsComponent} from "./components/components.component";
 import {RoutingsComponent} from "./routings/routings.component";
+import {NgClassesComponent} from "./ng-classes/ng-classes.component";
 
 export const routes: Routes = [
   { path: '', component: IntroductionComponent },
   { path: 'components', component: ComponentsComponent },
-  { path: 'routings', component: RoutingsComponent }
+  { path: 'routings', component: RoutingsComponent },
+  { path: 'ngclasses', component: NgClassesComponent }
+
 ];
